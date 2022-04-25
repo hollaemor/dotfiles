55c970c5a699533c77253f3c89b59bf3f39817e2
Emmanuel Oriarewo <hollaemor@gmail.com>
2022-04-25 12:45:30 +0200

    Minor change to cmp

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -123,8 +123,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
   },
   experimental = {
     ghost_text = false,
