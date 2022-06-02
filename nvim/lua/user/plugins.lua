ffd32a334ff14fc5995d447567527191667b6e66
Emmanuel Oriarewo <hollaemor@gmail.com>
2022-06-02 10:54:45 +0200

    Some styling

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,146 +1,147 @@
 local fn = vim.fn
 
 -- Automatically install Packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer. Close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer. Close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever plugins.lua is saved
-vim.cmd [[
+vim.cmd([[
  augroup packer_user_config
  autocmd!
  autocmd BufWritePost plugins.lua source <afile> | PackerSync
  augroup end
-]]
-
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Install plugins here
 
 return packer.startup(function(use)
-  -- plugins here
-  use "wbthomason/packer.nvim" -- let packer manage itself
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
+	-- plugins here
+	use("wbthomason/packer.nvim") -- let packer manage itself
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
 
-  -- colorschemes
-  use 'sainnhe/sonokai'
-  use 'sainnhe/edge'
-  use 'sainnhe/everforest'
-  use 'sainnhe/gruvbox-material'
-  use 'folke/tokyonight.nvim'
-  use 'marko-cerovac/material.nvim'
+	-- colorschemes
+	use("sainnhe/sonokai")
+	use("sainnhe/edge")
+	use("sainnhe/everforest")
+	use("sainnhe/gruvbox-material")
+	use("folke/tokyonight.nvim")
+	use("marko-cerovac/material.nvim")
 
-  -- Dev Icons
-  use 'kyazdani42/nvim-web-devicons'
+	-- Dev Icons
+	use("kyazdani42/nvim-web-devicons")
 
-  -- Nvim Tree
-  use 'kyazdani42/nvim-tree.lua'
+	-- Nvim Tree
+	use("kyazdani42/nvim-tree.lua")
 
-  -- Telescope
-  use "nvim-telescope/telescope.nvim"
+	-- Telescope
+	use("nvim-telescope/telescope.nvim")
 
-  -- Telescope extensions
-  use 'nvim-telescope/telescope-media-files.nvim'
+	-- Telescope extensions
+	use("nvim-telescope/telescope-media-files.nvim")
 
-  -- Treesitter
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
+	-- LSP
+	use("neovim/nvim-lspconfig") -- Collection of configurations for the built-in LSP client
+	use("williamboman/nvim-lsp-installer") -- language server installer
 
-  -- LSP
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
-  use 'williamboman/nvim-lsp-installer' -- language server installer
+	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+	-- Lualine
+	use("hoob3rt/lualine.nvim")
 
-  -- Lualine
-  use 'hoob3rt/lualine.nvim'
+	-- Bufferline
+	use("akinsho/bufferline.nvim")
 
-  -- Bufferline
-  use 'akinsho/bufferline.nvim'
+	use("lukas-reineke/indent-blankline.nvim") -- show indent lines in code
+	use("norcalli/nvim-colorizer.lua") -- show colours inline for hex values
 
+	-- completion plugins
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("hrsh7th/cmp-buffer") -- buffer completions
+	use("hrsh7th/cmp-path") -- path completions
+	use("hrsh7th/cmp-cmdline") -- cmdline completions
+	use("hrsh7th/cmp-nvim-lua")
 
-  use "lukas-reineke/indent-blankline.nvim" -- show indent lines in code
-  use 'norcalli/nvim-colorizer.lua' -- show colours inline for hex values
+	-- snippets
+	use("L3MON4D3/LuaSnip") --snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
+	use("windwp/nvim-autopairs") -- automatically close brackets and braces
 
-  -- completion plugins
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "hrsh7th/cmp-nvim-lua"
+	use("numToStr/Comment.nvim") -- Easily comment stuff
+	use("JoosepAlviste/nvim-ts-context-commentstring")
 
-  -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+	-- test runners
+	use("vim-test/vim-test")
+	use({ "rcarriga/vim-ultest", run = ":UpdateRemotePlugins" })
 
-  use 'windwp/nvim-autopairs' -- automatically close brackets and braces
+	use("hrsh7th/cmp-emoji") -- emogi supports
+	use("p00f/nvim-ts-rainbow") -- uses different colors for bracket pairs
 
-  use "numToStr/Comment.nvim" -- Easily comment stuff
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
+	-- programming language related
+	use("simrat39/rust-tools.nvim") --rust
+	use({ "ray-x/go.nvim", run = ":GoUpdateBinaries" }) -- Go
+	use("mfussenegger/nvim-jdtls") -- java lsp
+	use("ThePrimeagen/refactoring.nvim")
 
-  -- test runners
-  use 'vim-test/vim-test'
-  use { 'rcarriga/vim-ultest', run = ':UpdateRemotePlugins' }
+	-- database related
+	use("tpope/vim-dadbod") -- for interacting with DB
+	use("kristijanhusak/vim-dadbod-ui") -- UI for previous plugin
+	use("kristijanhusak/vim-dadbod-completion") -- lsp completions for db
 
-  use 'hrsh7th/cmp-emoji' -- emogi supports
-  use 'p00f/nvim-ts-rainbow' -- uses different colors for bracket pairs
+	-- Git specific
+	use("tpope/vim-fugitive")
+	use("sindrets/diffview.nvim")
+	use("lewis6991/gitsigns.nvim") -- show git lines in gutter
 
+	-- floating terminal
+	use({ "akinsho/toggleterm.nvim" })
 
-  -- programming language specific
-  use 'simrat39/rust-tools.nvim' --rust
-  use { 'ray-x/go.nvim', run = ":GoUpdateBinaries" } -- Go
-  use 'mfussenegger/nvim-jdtls' -- java lsp
+	-- utils
+	use("folke/which-key.nvim")
+	use("rcarriga/nvim-notify") -- cool notification
 
+	use({
+		"ur4ltz/surround.nvim",
+		config = function()
+			require("surround").setup({ mappings_style = "surround" })
+		end,
+	})
 
-
-  -- database related
-  use 'tpope/vim-dadbod' -- for interacting with DB
-  use 'kristijanhusak/vim-dadbod-ui' -- UI for previous plugin
-  use 'kristijanhusak/vim-dadbod-completion' -- lsp completions for db
-
-  -- Git specific
-  use 'tpope/vim-fugitive'
-  use 'sindrets/diffview.nvim'
-  use 'lewis6991/gitsigns.nvim' -- show git lines in gutter
-
-  -- floating terminal
-  use { "akinsho/toggleterm.nvim" }
-
-  -- utils
-  use "folke/which-key.nvim"
-  use 'rcarriga/nvim-notify' -- cool notification
-
-  -- Automatically setup config after cloning Packer repo
-  -- Should always be at end of other plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically setup config after cloning Packer repo
+	-- Should always be at end of other plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -1,46 +1,53 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-require 'telescope'.load_extension('media_files')
-telescope.setup {
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    preview = {
-      mime_hook = function(filepath, bufnr, opts)
-        local is_image = function(filepath)
-          local image_extensions = { 'png', 'jpg', 'jpeg' } -- Supported image formats
-          local split_path = vim.split(filepath:lower(), '.', { plain = true })
-          local extension = split_path[#split_path]
-          return vim.tbl_contains(image_extensions, extension)
-        end
-        if is_image(filepath) then
-          local term = vim.api.nvim_open_term(bufnr, {})
-          local function send_output(_, data, _)
-            for _, d in ipairs(data) do
-              vim.api.nvim_chan_send(term, d .. '\r\n')
-            end
-          end
+telescope.load_extension("media_files")
+telescope.load_extension("refactoring")
 
-          vim.fn.jobstart(
-            {
-              'kitty', '+kitten', 'icat', filepath -- Terminal image viewer command
-            },
-            { on_stdout = send_output, stdout_buffered = true })
-        else
-          require("telescope.previewers.utils").set_preview_message(bufnr, opts.winid, "Binary cannot be previewed")
-        end
-      end
-    },
-  },
-  extensions = {
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = {'mp4', 'webm', 'pdf'},
-      find_cmd = "rg" -- find command (defaults to `fd`)
-    }
-  }
-}
+telescope.setup({
+	defaults = {
+		prompt_prefix = " ",
+		selection_caret = " ",
+		preview = {
+			mime_hook = function(filepath, bufnr, opts)
+				local is_image = function(filepath)
+					local image_extensions = { "png", "jpg", "jpeg" } -- Supported image formats
+					local split_path = vim.split(filepath:lower(), ".", { plain = true })
+					local extension = split_path[#split_path]
+					return vim.tbl_contains(image_extensions, extension)
+				end
+				if is_image(filepath) then
+					local term = vim.api.nvim_open_term(bufnr, {})
+					local function send_output(_, data, _)
+						for _, d in ipairs(data) do
+							vim.api.nvim_chan_send(term, d .. "\r\n")
+						end
+					end
+
+					vim.fn.jobstart({
+						"kitty",
+						"+kitten",
+						"icat",
+						filepath, -- Terminal image viewer command
+					}, { on_stdout = send_output, stdout_buffered = true })
+				else
+					require("telescope.previewers.utils").set_preview_message(
+						bufnr,
+						opts.winid,
+						"Binary cannot be previewed"
+					)
+				end
+			end,
+		},
+	},
+	extensions = {
+		media_files = {
+			-- filetypes whitelist
+			-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+			filetypes = { "mp4", "webm", "pdf" },
+			find_cmd = "rg", -- find command (defaults to `fd`)
+		},
+	},
+})
