7a3816f0651b7251a371f9f06cc28fb6b0296aa9
Emmanuel Oriarewo <hollaemor@gmail.com>
2024-10-14 08:19:44 +0200

    Update Sway and NVIM configs

--- nvim/.config/nvim/lua/plugins/programming.lua
+++ nvim/.config/nvim/lua/plugins/programming.lua
@@ -1,35 +1,5 @@
 return {
   {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    config = function()
-      local rt = require "rust-tools"
-      local navic = require "nvim-navic"
-      require("core.utils").load_mappings "lspconfig"
-
-      rt.setup {
-        server = {
-          on_attach = function(client, bufnr)
-            navic.attach(client, bufnr)
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
-          end,
-        },
-      }
-    end,
-  },
-  {
-    "saecki/crates.nvim",
-    ft = { "rust", "toml" },
-
-    config = function(_, opts)
-      local crates = require "crates"
-      crates.setup(opts)
-      crates.show()
-    end,
-  },
-  {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
       "ray-x/guihua.lua",
@@ -44,13 +14,48 @@ return {
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
   {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
     },
-    config = function()
-      require("refactoring").setup()
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "graphql",
+        "helm",
+        "koto",
+        "go",
+        "gomod",
+        "diff",
+        "vimdoc",
+        "proto",
+        "dockerfile",
+        "json",
+        "xml",
+        "java",
+      })
     end,
   },
 }
