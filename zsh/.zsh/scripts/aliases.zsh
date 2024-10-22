4559bdcc34c3a86543eb5c7e339a0aa273423b8f
Emmanuel Oriarewo <hollaemor@gmail.com>
2024-10-22 21:00:49 +0200

    Add more aliases

--- zsh/.zsh/scripts/aliases.zsh
+++ zsh/.zsh/scripts/aliases.zsh
@@ -2,3 +2,8 @@ alias v="nvim"
 alias ls="exa --icons"
 alias mine="cd ~/Documents/code/github.com/hollaemor/"
 alias cone="cd ~/Documents/code/gitlab.com/cargoone/"
+
+alias services="cd ~/Documents/code/gitlab.com/cargoone/services/"
+alias frontends="cd ~/Documents/code/gitlab.com/cargoone/frontends/"
+alias tools="cd ~/Documents/code/gitlab.com/cargoone/tools/"
+alias libraries="cd ~/Documents/code/gitlab.com/cargoone/libraries/"
