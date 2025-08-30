c0da3545915aa1910d876a86817bc79141a186bf
Emmanuel <hollaemor@gmail.com>
2025-08-30 20:13:22 +0200

    Add new NVIM plugins

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/emor/.config/omarchy/current/theme/neovim.lua
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -7,10 +7,10 @@ return {
   },
 
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
 }
