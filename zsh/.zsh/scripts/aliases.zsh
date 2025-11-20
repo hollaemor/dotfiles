5dd2007b21cd88aa83776a046fb50b327701ceda
Emmanuel Oriarewo <hollaemor@gmail.com>
2025-11-20 17:07:56 +0100

    Increase Kitty font. Also add  alias

--- zsh/.zsh/scripts/aliases.zsh
+++ zsh/.zsh/scripts/aliases.zsh
@@ -7,3 +7,19 @@ alias cone="cd ~/Documents/code/gitlab.com/cargoone/"
 # alias frontends="cd ~/Documents/code/gitlab.com/cargoone/frontends/"
 # alias tools="cd ~/Documents/code/gitlab.com/cargoone/tools/"
 # alias libraries="cd ~/Documents/code/gitlab.com/cargoone/libraries/"
+
+# Auto-activate Python virtual environment on directory change
+function chpwd() {
+    # Deactivate any current virtual environment
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        deactivate
+    fi
+
+    # Check for common virtual environment directories
+    for venv_dir in .venv venv env virtualenv; do
+        if [[ -f "$venv_dir/bin/activate" ]]; then
+            source "$venv_dir/bin/activate"
+            break
+        fi
+    done
+}
